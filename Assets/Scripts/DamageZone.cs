286b9e9b3473b55511fc512bee699096fb8bc38c
tyrannnnnt <1095144689@qq.com>
2021-01-18 18:40:35 +0800

    提交文件

---
+++ Assets/Scripts/DamageZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+        UnityEngine.Debug.Log("touched");
+        if (controller != null)
+        {
+            UnityEngine.Debug.Log("damaged");
+            controller.ChangeHealth(-1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+
+    bool broken = true;
+
+    public ParticleSystem smokeEffect;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!broken)
+        {
+            return;
+        }
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!broken)
+        {
+            return;
+        }
+
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical)
+        {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
+
+            position.y = position.y + Time.deltaTime * speed * direction;;
+        }
+        else
+        {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;;
+        }
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        smokeEffect.Stop();
+    }
+}
